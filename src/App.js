f299321cee500eba78607ed986c17cb54463b23f
Hannibal <88138693+HeyHannibal@users.noreply.github.com>
2022-03-11 00:23:32 +0300

    added rounds, fetch targets, and timer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import { useRef, useState, useEffect } from "react";
 
 // firebase
+import { initializeApp } from "firebase/app";
+import { getDoc, getFirestore } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  addDoc,
+  getDocs,
+} from "firebase/firestore";
 
+// components
 import img1 from "./assets/5.jpg";
 import StartGame from "./components/startSreen";
 import UI from "./components/ui";
 import Viewfinder from "./components/viewfinder";
 import LoadAnimation from "./components/animations";
 import Timer from "./components/timer";
-import Leaderboard from "./components/leaderboard";
 
-import { initializeApp } from "firebase/app";
-import { getDoc, getFirestore } from "firebase/firestore";
-import {
-  collection,
-  doc,
-  setDoc,
-  addDoc,
-  getDocs,
-  deleteDoc,
-  updateDoc,
-  Timestamp,
-} from "firebase/firestore";
 
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAeIFD5PXQCHqo-AD45_GLzp5yRsJ403dA",
   authDomain: "phototagging-47506.firebaseapp.com",
@@ -31,37 +28,39 @@ const firebaseConfig = {
   messagingSenderId: "196085090300",
   appId: "1:196085090300:web:78eeebade8eee855a09479",
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-async function enterLeaderboard(name, score) {
-  addDoc(collection(db, "leaderboard"), {
-    name: name,
-    score: score,
-  });
-}
-
 function App() {
-  const [gameState, setGameState] = useState({
-    found: [],
-    isOver: false,
-    isOn: false,
-    round: 0,
-  });
+  
+  async function getCollection(name) {
+    let getCollection = await getDocs(collection(db, name));
+    let allDocs = getCollection.docs.map((doc) => {
+      return doc.data();
+    });
+    return allDocs;
+  }
+ async function startStopTimer(id, end) {
+    if (arguments.length === 0) {
+    let userRef = await addDoc(collection(db, "myTime"), {
+        startTimestamp: new Date().getTime(),
+      });
+      setTimerID(userRef._key.path.segments[1]);
+    } 
+     else {
+      let start = await (await getDoc(doc(db, "myTime", id))).data()
+        .startTimestamp;
+      setUserTimeScore((end - start) / 1000);
+  }}
+  
   const [round, setRound] = useState(0);
   const [found, setFound] = useState([]);
   const [targets, setTargets] = useState(null);
-
   const [timerDatabaseId, setTimerID] = useState(null);
   const [userTimeScore, setUserTimeScore] = useState(null);
-  ///game animations
   const [openFrame, setOpenFrame] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [foundOne, setFoundOne] = useState(false);
-
   const [imgWidth, setImgWidth] = useState(0);
   const [X, setX] = useState(0);
   const [Y, setY] = useState(0);
@@ -71,126 +70,56 @@ function App() {
   const imgParent = useRef(null);
 
   useEffect(async () => {
-    // console.log(found);
-    if (found.length === 3 && round < 2) {
-      setFound([]);
-      setRound((prev) => prev + 1);
-    }
+    if (found.length === 3) {
+      if (round < targets.length - 1) {
+        setFound([]);
+        setRound((prev) => prev + 1);
+      } else {
+        let end = new Date().getTime();
 
-    //alert("gameover");
-    //stopTimer(timerDatabaseId);
+        console.log("over");
+        startStopTimer(timerDatabaseId, end);
+      }
+    }
 
     if (!targets) {
-      let targets = [
-        [
-          {
-            rangeX: [90, 98],
-            rangeY: [66, 68],
-            name: "predator",
-          },
-          {
-            name: "saw dude",
-            rangeX: [66, 72],
-            rangeY: [90, 91],
-          },
-          {
-            rangeY: [80, 81],
-            rangeX: [7, 13],
-            name: "jay and silent bob",
-          },
-        ],
-        [
-          {
-            name: "bender",
-            rangeX: [18, 22],
-            rangeY: [80, 85],
-          },
-          {
-            name: "afrosamurai",
-            rangeX: [49, 53],
-            rangeY: [70, 75],
-          },
-          {
-            name: "mask from spirited away",
-            rangeX: [59, 63],
-            rangeY: [35, 38],
-          },
-        ],
-      ];
-      //  let targets = await getCollection("targets");
+      const getTargets = await getCollection("targets");
+      const targets = Object.values(getTargets[0].targets);
+
+      const getRange = (x, y) => {
+        return { rangeY: [y - 10, y + 11], rangeX: [x - 30, x + 30] };
+      };
+      for (const arr of targets) {
+        arr.map((item) =>
+          Object.assign(item, getRange(item.coords[0], item.coords[1]))
+        );
+      }
+      console.log(targets)
       setTargets(targets);
     }
-  }, [gameState, isLoading, foundOne]);
-
-  async function getCollection(name) {
-    let getCollection = await getDocs(collection(db, name));
-    let allDocs = getCollection.docs.map((doc) => {
-      return doc.data();
-    });
-    return allDocs;
-  }
-
-  async function startGame() {
-    startTimer();
-  }
-  async function startTimer() {
-    let start = Timestamp.now();
-    let userRef = await addDoc(collection(db, "myTime"), {
-      startTimestamp: start["nanoseconds"],
-    });
-    setTimerID(userRef._key.path.segments[1]);
-  }
-  async function stopTimer(id) {
-    let start = await (await getDoc(doc(db, "myTime", id))).data()
-      .startTimestamp;
-    let end = Timestamp.now().nanoseconds;
-    setUserTimeScore((end - start) / 1_000_000 / 10);
-  }
+  }, [isLoading, found, round]);
 
   function clickAndFind(e) {
     setOpenFrame(true);
     const { width, height } = e.target.getBoundingClientRect();
     const { offsetX, offsetY } = e.nativeEvent;
     const { pageX, pageY } = e;
-
-    // let x = Math.round((offsetX / width) * 100);
-    // let y = Math.round((offsetY / height) * 100);
-    // console.log(x, y);
-
-  
     setImgWidth(width);
-
     setX(pageX);
     setY(pageY);
-
-  let nx = Math.round((offsetX / width) * 1000);
-    let ny = Math.round((offsetY / height) * 1000);
-
-    setClickX(nx);
-    setClickY(ny);
-
-
-
-    function calc(nx, ny) {
-      return {
-        rangeY: [ny - 10, ny + 11],
-        rangeX: [nx - 30, nx + 30],
-      };
-    }
-
-    // if (
-    //   nx >= newTarg.rangeX[0] &&
-    //   nx <= newTarg.rangeX[1] &&
-    //   ny >= newTarg.rangeY[0] &&
-    //   ny <= newTarg.rangeY[1]
-    // )
-    //   console.log("true");
+    let x = Math.round((offsetX / width) * 1000);
+    let y = Math.round((offsetY / height) * 1000);
+    setClickX(x);
+    setClickY(y);
+    (function allc(x, y) {
+      console.log([x, y]);
+    })(x, y);
+    console.log(timerDatabaseId);
   }
 
   async function tagPhoto(e) {
     let indx = Number(e.currentTarget.id);
     const eTargetName = e.currentTarget.textContent.toLowerCase();
-
     const { rangeX, rangeY, name } = targets[round][indx];
     if (
       clickX >= rangeX[0] &&
@@ -200,13 +129,21 @@ function App() {
     ) {
       if (eTargetName === name) {
         if (!found.includes(name)) {
-          // console.log("true");
+          console.log("true");
 
           setFound((prev) => [...prev, name]);
         } else console.log("wrong");
       }
     }
   }
+
+  function complete() {
+    let allTargos = targets[round].map((item) => {
+      return item.name;
+    });
+    setFound(allTargos);
+  }
+
   const frameStyle = {
     width: (imgWidth / 100) * 10,
     height: (imgWidth / 100) * 10,
@@ -216,10 +153,13 @@ function App() {
     top: Y - ((imgWidth / 100) * 10) / 2,
     left: X - ((imgWidth / 100) * 10) / 2,
   };
+
   return (
     <div className="App" ref={imgParent}>
-      {targets ? <UI targets={targets[round]} found={found} /> : null}
-      <StartGame start={startGame} />
+      {targets ? (
+        <UI targets={targets[round]} found={found} finish={complete} />
+      ) : null}
+      <StartGame start={startStopTimer} />
       {userTimeScore ? (
         <Timer
           myTime={userTimeScore}
@@ -249,28 +189,3 @@ function App() {
 }
 
 export default App;
-
-// let targos = [
-//   {
-//     rangeX: [90, 98],
-//     rangeY: [66, 68],
-//     name: "predator",
-//   },
-//   {
-//     name: "saw dude",
-//     rangeX: [66, 72],
-//     rangeY: [90, 91],
-//   },
-//   {
-//     rangeY: [80, 81],
-//     rangeX: [7, 13],
-//     name: "jay and silent bob",
-//   },
-// ];
-
-// // async function getTarget(id) {
-// //   setIsLoading(true);
-// //   let target = await getDoc(doc(db, "targets", id));
-// //   setIsLoading(false);
-// //   return target.data();
-// // }
--- src/components/ui.js
+++ src/components/ui.js
@@ -1,12 +1,15 @@
 import { seState, useEffect } from "react";
 import React from 'react'
 export default function UI(props) {
-  // useEffect(() => {
-  //   console.log(props)
-  // },[props])
+  useEffect(() => {
+    console.log(props)
+  },[props])
   
+  const finish = () => props.finish()
+
   return (
     <div id='uiBar'>
+      <button onClick={finish}>Finish Game</button>
       <ul className='uiBar ' id='Targets'>
         {props.targets.map((target) =>
           <li key={target['name']} className='targetLi'>
